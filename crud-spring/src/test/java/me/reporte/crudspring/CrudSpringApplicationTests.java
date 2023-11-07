88c7548ed7164c062ca9935798a094b184fdd3f2
Jônatas Santos <jts.msantos@gmail.com>
2023-11-07 11:33:07 -0300

    feat: add spring crud for backend

---
+++ crud-spring/src/test/java/me/reporte/crudspring/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.reporte.crudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
